3db34ea9b6e1441aceae3a923c8f706dbfcd859a
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2024-03-03 19:28:50 -0800

    Started the prompting

--- src/makefile-tool.sh
+++ src/makefile-tool.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+prompt() {
+	name_arg=()
+
+	read -p "Please enter a name for your executable: " name
+	name_arg+=($name)
+
+	echo ${name_arg[0]}
+	
+	while read -r item; do 
+		if [[ -z "$item" ]]; then
+			break
+		fi
+
+		echo $item
+	done
+}
+
 # list all the cpp files in files array.
 print_files() {
 
@@ -27,8 +44,10 @@ get_files() {
 		fi
 	done
 
+	# if nothing was read to the files array
 	if [[ ${#files[@]} =~ 0 ]]; then
-		echo No .cpp files were found in "$directory". Did you specify the right directory?
+		echo No .cpp files were found in \""$directory"\" 
+		echo Did you specify the right directory?
 		exit 1
 	fi	
 
@@ -48,6 +67,7 @@ check_argument() {
 files=()
 directory=$1
 
+# if the number of arguments is not one
 if [ $# -ne 1 ]; then
 	echo No argument specified or too many specified. 
 	echo Usage: bash makefile-tool.sh \"directory\"
@@ -57,3 +77,4 @@ fi
 check_argument 
 get_files
 print_files
+prompt
