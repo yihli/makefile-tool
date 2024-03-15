ba383d7ae9370c112ed0d2157039c98f91eef89b
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2024-03-15 02:33:59 -0700

    Added infinite loop

--- src/makefile-tool.sh
+++ src/makefile-tool.sh
@@ -26,20 +26,23 @@ check_for_main() {
 prompt() {
 	exec_names=()
 
-	read -p "Please enter a name for your executable: " name
-	exec_names+=($name)
+	while true
+	do
+		read -p "Please enter a name for your executable: " name
+		exec_names+=($name)
 
-	echo Your executable will be named \"${name}\"
-	
-	read -r -p "Please specify which .cpp file contains \"int main()\": " chosen_file
+		echo Your executable will be named \"${name}\"
+		
+		read -r -p "Please specify which .cpp file contains \"int main()\": " chosen_file
 
-	# if there was no user input
-	if [[ -z "$chosen_file" ]]; then
-		echo No file was specified. Exiting...
-		exit 1
-	fi
+		# if there was no user input
+		if [[ -z "$chosen_file" ]]; then
+			echo No file was specified. Exiting...
+			exit 1
+		fi
 
-	check_for_main ${exec_names[0]}
+		check_for_main ${name}
+	done
 
 }
 
