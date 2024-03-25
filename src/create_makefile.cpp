ce305e58a7d7b1eff16e493c39d4198ada08b6b2
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2024-03-25 12:03:41 -0700

    Added comments to createMakefile

--- src/create_makefile.cpp
+++ src/create_makefile.cpp
@@ -3,6 +3,15 @@
 #include <string>
 #include <set>
 
+/*
+ * Generates the Makefile template and adds the lines needed to generate the 
+ * executable. Makefile is created in the given directory.
+ * 
+ * @param header_names: the set of header names to include
+ * @param exec_name: the name of the executable
+ * @param dir: the directory to place the Makefile in
+ * 
+ */
 void createMakefile(std::set<std::string>& header_names, std::string exec_name, std::string dir) {
     // creating Makefile file. overwrites existing Makefile.
     std::ofstream file(dir + "Makefile");
@@ -13,6 +22,7 @@ void createMakefile(std::set<std::string>& header_names, std::string exec_name, 
 
     file << exec_name << ": ";
     
+    // this adds all the associated header names to the Makefile
     for (auto iter = header_names.begin(); iter != header_names.end(); iter++) {
         file << *iter << " ";
     }
